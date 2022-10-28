1e383cbc89b59bce0db896d97fa8a7e71d1f04c6
Hensi Solanki <hensisolanki@logisticinfotech.co.in>
2022-10-28 12:46:32 +0530

    migrations for crud

---
+++ database/migrations/2022_10_04_221048_create_lots_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lots', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->dateTime('date');
+            $table->double('startAmount');
+            $table->integer('uid');
+            $table->boolean('status')->default(false);
+            $table->string('customFields')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lots');
+    }
+}
