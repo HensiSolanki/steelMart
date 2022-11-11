b7371f5582a17c5ac62c38442013f4068c6814c6
ravisuani <114324116+ravisuani@users.noreply.github.com>
2022-11-12 00:02:36 +0530

    Materials Fields Added

--- resources/views/admin/materials/create.blade.php
+++ resources/views/admin/materials/create.blade.php
@@ -21,24 +21,80 @@
                                 <div class="row">
                                     <label for="title" class="col-sm-2 col-form-label">Title</label>
                                     <div class="col-sm-7">
-                                        <input type="text" class="form-control" name="starting_price"
-                                            placeholder="Auction Price" autocomplete="off" autofocus>
+                                        <input type="text" class="form-control" id="title" name="title"
+                                            placeholder="Title" autocomplete="off" autofocus>
                                     </div>
                                 </div>
                                 <div class="row">
-                                    <label for="title" class="col-sm-2 col-form-label">Last Bid</label>
+                                    <label for="description" class="col-sm-2 col-form-label">Description</label>
                                     <div class="col-sm-7">
-                                        <input type="text" class="form-control" name="last_bid"
-                                            placeholder="Last Bid" autocomplete="off" autofocus>
+                                        <textarea type="text" class="form-control" id="description" name="description" placeholder="Description" autofocus></textarea>
                                     </div>
                                 </div>
                                 <div class="row">
+                                    <label for="categoryId" class="col-sm-2 col-form-label">Category</label>
+                                    <div class="col-sm-7">
+                                        <select class="form-select" id="categoryId" name="categoryId">
+                                            @foreach ($categorys as $category)
+                                                <option value={{ $category->id }}
+                                                    @if (!$addForm && $materials->categoryId == $category->id) selected @endif>{{ $category->title }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label for="height" class="col-sm-2 col-form-label">Height</label>
+                                    <div class="col-sm-7">
+                                        <input type="number" class="form-control" id="height" name="height"
+                                            placeholder="Height" autocomplete="off" autofocus>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label for="width" class="col-sm-2 col-form-label">Width</label>
+                                    <div class="col-sm-7">
+                                        <input type="number" class="form-control" id="width" name="width"
+                                            placeholder="Width" autocomplete="off" autofocus>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label for="length" class="col-sm-2 col-form-label">Length</label>
+                                    <div class="col-sm-7">
+                                        <input type="number" class="form-control" id="length" name="length"
+                                            placeholder="Length" autocomplete="off" autofocus>
+                                    </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <label for="weight" class="col-sm-2 col-form-label">Weight</label>
+                                    <div class="col-sm-7">
+                                        <input type="number" class="form-control" id="weight" name="weight"
+                                            placeholder="Weight" autocomplete="off" autofocus>
+                                    </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <label for="inStock" class="col-sm-2 col-form-label">Stock</label>
+                                    <div class="col-sm-7">
+                                        <input type="number" class="form-control" id="inStock" name="inStock"
+                                            placeholder="Stock" autocomplete="off" autofocus>
+                                    </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <label for="price" class="col-sm-2 col-form-label">Price</label>
+                                    <div class="col-sm-7">
+                                        <input type="number" class="form-control" id="price" name="price"
+                                            placeholder="Price" autocomplete="off" autofocus>
+                                    </div>
+                                </div>
+                                {{-- <div class="row">
                                     <label for="title" class="col-sm-2 col-form-label">Auction Date</label>
                                     <div class="col-sm-7">
                                         <input type="date" class="form-control" name="auction_date"
                                             placeholder="Ingrese el post title" autocomplete="off" autofocus>
                                     </div>
-                                </div>
+                                </div> --}}
                                 {{-- <div class="row">
                                     <label for="title" class="col-sm-2 col-form-label">Auction</label>
                                     <div class="col-sm-7">
