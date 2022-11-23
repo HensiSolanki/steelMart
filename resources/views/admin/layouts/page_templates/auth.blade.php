408aa8d9c9e70dc947c7470b9309344f94767df8
Hensi Solanki <hensisolanki@gmail.com>
2022-11-23 23:09:40 +0530

    add folder

--- resources/views/admin/layouts/page_templates/auth.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="wrapper ">
-  @include('admin.layouts.navbars.sidebar')
-  <div class="main-panel">
-    @include('admin.layouts.navbars.navs.auth')
-      @yield('content')
-    @include('admin.layouts.footers.auth')
-  </div>
-</div>
--- resources/views/seller/layouts/page_templates/auth.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="wrapper ">
-  @include('seller.layouts.navbars.sidebar')
-  <div class="main-panel">
-    @include('seller.layouts.navbars.navs.auth')
-      @yield('content')
-    @include('seller.layouts.footers.auth')
-  </div>
-</div>
